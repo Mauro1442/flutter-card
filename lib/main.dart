e2e67f0edeb6f8dc795970aad24c22cce7db007f
User <user@MBP-von-User.fritz.box>
2022-10-14 18:15:59 +0200

    first repo

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Container(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.only(left: 30),
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
