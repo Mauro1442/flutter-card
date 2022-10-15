f761f4e2875d82cb87c50940f297259aa4ab8016
User <user@MBP-von-User.fritz.box>
2022-10-15 02:01:38 +0200

    final repo

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,76 @@ class MyApp extends StatelessWidget {
     return     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(
-          child: Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.only(left: 30),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Text('Hello'),
-          ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/mauro.jpeg"),
+              ),
+              Text(
+                  "Mauro Di Rosa",
+                  style: TextStyle(
+                    fontFamily: "Pacifico",
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "SourceSansPro",
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade900,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal:  25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+49 157 39628651",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal.shade900,
+                      fontFamily: "SourceSansPro"
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal:  25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "maurodirosa19@gmail.com",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                        fontFamily: "SourceSansPro"
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
         ),
       ),
     );
